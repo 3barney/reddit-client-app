bcc63fd1563277c7eb4d9074f5e0bf5647aff55d
barney <brnbsjm@gmail.com>
2019-01-23 11:46:41 +0300

    App setup with toolbar component

---
+++ app/src/main/java/events254/wakenyawawili/com/kedditbysteps/MainActivity.kt
@@ -0,0 +1,17 @@
+package events254.wakenyawawili.com.kedditbysteps
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.support.v7.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+    }
+}
